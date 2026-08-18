8ddb7b14d1ed61854125052b21d715d6ec9890de
gean1080gc-netizen <gean1080.gc@gmail.com>
2026-08-18 09:50:42 -0300

    Update MainActivity.kt

--- app/src/main/java/com/copiloto/auto/MainActivity.kt
+++ app/src/main/java/com/copiloto/auto/MainActivity.kt
@@ -1,10 +1,18 @@
 package com.copiloto.auto
 
 import android.os.Bundle
+import android.widget.TextView
 import androidx.appcompat.app.AppCompatActivity
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        
+        val textView = TextView(this).apply {
+            text = "Copiloto Auto Ativo!"
+            textSize = 22f
+            setPadding(60, 60, 60, 60)
+        }
+        setContentView(textView)
     }
 }

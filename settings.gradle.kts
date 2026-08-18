fb641a8025ca4283cad0bfd7ee50207e776276d5
gean1080gc-netizen <gean1080.gc@gmail.com>
2026-08-17 21:56:38 -0300

    Update settings.gradle.kts

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -5,5 +5,13 @@ pluginManagement {
         gradlePluginPortal()
     }
 }
+dependencyResolutionManagement {
+    repositoriesMode.set(RepositoriesMode.FAIL_ON_PROJECT_REPOS)
+    repositories {
+        google()
+        mavenCentral()
+    }
+}
+
 rootProject.name = "CopilotoAuto"
-include(":app")
+include(":aplicativo")
